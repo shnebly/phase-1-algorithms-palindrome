b8cf47f8b9cd17221dc6c2aeb0cf0aa0b3657822
Shelby Morris <shelbynicole.morris@gmail.com>
2023-03-16 05:17:33 -0600

    one method to reverse

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let reversedWord = word.split('').reverse().join('')
+  return reversedWord === word
 }
 
 /* 
-  Add your pseudocode here
+  name a function with one parameter
+  take that parameter and reverse it
+  there is a built in array method .reverse()
+  in order for this to work you need to .split('') to create an array from the word
+  .join('') the split array as a reversed word and compare the two
+  return a boolean
 */
 
 /*
-  Add written explanation of your solution here
+  this function should take a string as input and a produce a boolean as output
+  it will be true if the string letters are in the same order forwards as they are backwards
+  can i reverse the string and compare the two?
+  can i iterate through each letter, find the middle index (rounded up), compare the string from index[0] to middle index and from index[length-1] to middle index?
+
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+  console.log("=>", isPalindrome("mom"));
+  console.log("=>", isPalindrome("lololol"));
 
   console.log("");
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  console.log("=>", isPalindrome("shelby"));
 }
 
 module.exports = isPalindrome;
